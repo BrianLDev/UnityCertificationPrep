dd195d23956a7caa676ee5fe1f82a91c0d4e0b5d
Brian Leip <brianleip@gmail.com>
2017-03-03 00:36:35 -0800

    Section 13: Animations completed

--- 11-Physics/Assets/Physics Asset Package/Aiming/BallisticsAim.cs
+++ 11-Physics/Assets/Physics Asset Package/Aiming/BallisticsAim.cs
@@ -14,7 +14,7 @@ public class BallisticsAim : MonoBehaviour {
 	void Update () {
         var ray = new Ray(transform.position, transform.forward);
         RaycastHit hitInfo;
-        if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity, 1 << 8));
+        if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity, 1 << 8));     // for the 8th layer, need to enter 1 shifted by 8 bits (i.e. 1 << 8)
         {
             print(hitInfo.point);
             solver.target = hitInfo.point;
