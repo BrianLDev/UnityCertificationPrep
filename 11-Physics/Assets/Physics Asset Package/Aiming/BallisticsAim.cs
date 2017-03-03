eedd4c3d6a869bb25371ecaa0bb2ea6e03100f5a
Brian Leip <brianleip@gmail.com>
2017-03-02 18:14:31 -0800

    Section 11: Physics completed

---
+++ 11-Physics/Assets/Physics Asset Package/Aiming/BallisticsAim.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallisticsAim : MonoBehaviour {
+
+    public BallisticSolver solver;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity, 1 << 8));
+        {
+            print(hitInfo.point);
+            solver.target = hitInfo.point;
+        }
+    }
+}
